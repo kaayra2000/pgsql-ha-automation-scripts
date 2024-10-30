5f8d6ca998108e7e8b79b385c9d17ed9e900b894
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2024-10-30 11:56:14 +0300

    keepalived fonksiyonlarıyla ayrı bir klasöre taşındı

---
+++ keepalived_scripts/argument_parser.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Varsayılan değerler
+DEFAULT_INTERFACE="enp0s3"
+DEFAULT_SQL_VIRTUAL_IP="10.207.80.20"
+DEFAULT_DNS_VIRTUAL_IP="10.207.80.30"
+DEFAULT_PRIORITY="100"
+DEFAULT_STATE="BACKUP"
+DEFAULT_SQL_CONTAINER="sql_container"
+DEFAULT_DNS_CONTAINER="dns_container"
+DOCKER_BINARY_PATH="/usr/bin/docker"
+
+parse_arguments() {
+    # Varsayılan değişkenleri dışa aktarma
+    export INTERFACE=$DEFAULT_INTERFACE
+    export SQL_VIRTUAL_IP=$DEFAULT_SQL_VIRTUAL_IP
+    export DNS_VIRTUAL_IP=$DEFAULT_DNS_VIRTUAL_IP
+    export PRIORITY=$DEFAULT_PRIORITY
+    export STATE=$DEFAULT_STATE
+    export SQL_CONTAINER=$DEFAULT_SQL_CONTAINER
+    export DNS_CONTAINER=$DEFAULT_DNS_CONTAINER
+
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --interface)
+                INTERFACE="$2"
+                shift 2
+                ;;
+            --sql-virtual-ip)
+                SQL_VIRTUAL_IP="$2"
+                shift 2
+                ;;
+            --dns-virtual-ip)
+                DNS_VIRTUAL_IP="$2"
+                shift 2
+                ;;
+            --priority)
+                PRIORITY="$2"
+                shift 2
+                ;;
+            --state)
+                if [[ "$2" != "MASTER" && "$2" != "BACKUP" ]]; then
+                    echo "Hata: State sadece MASTER veya BACKUP olabilir."
+                    exit 1
+                fi
+                STATE="$2"
+                shift 2
+                ;;
+            --sql-container)
+                SQL_CONTAINER="$2"
+                shift 2
+                ;;
+            --dns-container)
+                DNS_CONTAINER="$2"
+                shift 2
+                ;;
+            *)
+                echo "Bilinmeyen argüman: $1"
+                exit 1
+                ;;
+        esac
+    done
+}
--- keepalived_scripts/create_keepalived.sh
+++ keepalived_scripts/create_keepalived.sh
@@ -1,65 +1,9 @@
 #!/bin/bash
+# Scriptin bulunduğu dizini alma
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Varsayılan değerler
-DEFAULT_INTERFACE="enp0s3"
-DEFAULT_SQL_VIRTUAL_IP="10.207.80.20"
-DEFAULT_DNS_VIRTUAL_IP="10.207.80.30"
-DEFAULT_PRIORITY="100"
-DEFAULT_STATE="BACKUP"
-DEFAULT_SQL_CONTAINER="sql_container"
-DEFAULT_DNS_CONTAINER="dns_container"
-DOCKER_BINARY_PATH="/usr/bin/docker"
-# Argüman ayrıştırma fonksiyonu
-parse_arguments() {
-    INTERFACE=$DEFAULT_INTERFACE
-    SQL_VIRTUAL_IP=$DEFAULT_SQL_VIRTUAL_IP
-    DNS_VIRTUAL_IP=$DEFAULT_DNS_VIRTUAL_IP
-    PRIORITY=$DEFAULT_PRIORITY
-    STATE=$DEFAULT_STATE
-    SQL_CONTAINER=$DEFAULT_SQL_CONTAINER
-    DNS_CONTAINER=$DEFAULT_DNS_CONTAINER
-
-    while [[ $# -gt 0 ]]; do
-        case $1 in
-            --interface)
-                INTERFACE="$2"
-                shift 2
-                ;;
-            --sql-virtual-ip)
-                SQL_VIRTUAL_IP="$2"
-                shift 2
-                ;;
-            --dns-virtual-ip)
-                DNS_VIRTUAL_IP="$2"
-                shift 2
-                ;;
-            --priority)
-                PRIORITY="$2"
-                shift 2
-                ;;
-            --state)
-                if [[ "$2" != "MASTER" && "$2" != "BACKUP" ]]; then
-                    echo "Hata: State sadece MASTER veya BACKUP olabilir."
-                    exit 1
-                fi
-                STATE="$2"
-                shift 2
-                ;;
-            --sql-container)
-                SQL_CONTAINER="$2"
-                shift 2
-                ;;
-            --dns-container)
-                DNS_CONTAINER="$2"
-                shift 2
-                ;;
-            *)
-                echo "Bilinmeyen argüman: $1"
-                exit 1
-                ;;
-        esac
-    done
-}
+# Parse dosyasını script dizinine göre import etme
+source "$SCRIPT_DIR/argument_parser.sh"
 # Docker komutunu çalıştırma yetkisini kontrol etme ve ekleme
 check_and_add_docker_permissions() {
     if ! groups keepalived_script | grep -q docker; then
@@ -252,7 +196,7 @@ EOF'
     fi
 }
 
-# Ana script
+# Parse fonksiyonunu çağırma ve sonuçları alma
 parse_arguments "$@"
 create_keepalived_user
 setup_container_log $SQL_CONTAINER
