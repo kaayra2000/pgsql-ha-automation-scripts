638f69a5766506a31bbcb094538362b81ef88c89
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2024-11-21 15:56:57 +0300

    gereksiz haproxy argümanları kaldırldı

--- haproxy_scripts/create_haproxy.sh
+++ haproxy_scripts/create_haproxy.sh
@@ -6,5 +6,5 @@ source $SCRIPT_DIR/../argument_parser.sh # argument_parser.sh dosyasındaki fonk
 source $SCRIPT_DIR/../general_functions.sh
 check_and_parse_arguments $ARGUMENT_CFG_FILE
 ha_proxy_kur
-ha_proxy_konfigure_et $NODE1_IP $NODE2_IP $HAPROXY_BIND_PORT $POSTGRES_BIND_PORT $PGSQL_PORT $HAPROXY_PORT
+ha_proxy_konfigure_et
 enable_haproxy
