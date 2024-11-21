12d1e022e38d13d80800aacc0d8385d5cdb0b963
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2024-11-21 14:17:36 +0300

    gereksiz dosyalar silindi

--- ip_ayari_sifirla.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source fonksiyonlar.sh
-source varsayilan_degiskenler.sh # varsayilan_degiskenler.sh dosyasındaki değişkenleri kullanmak için
-
-# internetin gelmesi için virtualbox ayarlarından interneti NAT seçmek gerekiyor
-
-ip_sifirla
