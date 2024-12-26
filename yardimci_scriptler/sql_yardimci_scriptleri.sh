a828fa0b54efeeccd60f61cd0b6e0b468f49bea8
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2024-12-26 11:27:48 +0300

    sql loglarını çekme ve yazdırma yardımcı fonksiyonları eklendi

--- yardimci_scriptler/sql_yardimci_scriptleri.sh
+++ yardimci_scriptler/sql_yardimci_scriptleri.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-
-# Docker konteynırında çalışan patroni servisinin loglarını getirme
-get_patroni_log() {
+# SQL server'a bağlanabilmek için
+connect_sql_server() {
+    local SQL_IP=${1:-"10.207.80.20"}
+    psql -U postgres -h $SQL_IP
+}
+# Docker konteynırında çalışan Patroni servisinin loglarını ekrana yazdırma
+print_patroni_log() {
     docker exec sql_container /bin/bash -c "cat /var/log/patroni/patroni.log"
 }
 
+# Docker konteynırında çalışan PostgreSQL loglarını ekrana yazdırma
+print_sql_log() {
+    docker exec sql_container /bin/bash -c "ls -tr /var/lib/postgresql/16/data/log | while read file; do cat /var/lib/postgresql/16/data/log/\$file; done"
+}
+
+# Patroni loglarını bulunduğunuz klasöre kopyalama
+get_patroni_log() {
+    docker exec sql_container /bin/bash -c "cat /var/log/patroni/patroni.log" > patroni.log
+    echo "Patroni logları bulunduğunuz klasöre 'patroni.log' olarak kaydedildi."
+}
+
+# PostgreSQL loglarını bulunduğunuz klasöre kopyalama
+get_sql_log() {
+    docker exec sql_container /bin/bash -c "ls -tr /var/lib/postgresql/16/data/log | while read file; do cat /var/lib/postgresql/16/data/log/\$file; done" > sql_logs.log
+    echo "PostgreSQL logları bulunduğunuz klasöre 'sql_logs.log' olarak kaydedildi."
+}
 
 # SQL server konteynır'ını başlatma
 start_sql_container() {
@@ -19,8 +39,3 @@ stop_sql_container() {
     docker stop sql_container
 }
 
-# SQL server'a bağlanabilmek için
-connect_sql_server() {
-    local SQL_IP=${1:-"10.207.80.20"}
-    psql -U postgres -h $SQL_IP
-}
