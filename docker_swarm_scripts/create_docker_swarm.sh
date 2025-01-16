05060c9b21ec87403edef950156e70e1185d2617
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2025-01-16 14:22:34 +0300

    docker swarm worker token arguments.cfg dosyasına yazılıyor

--- docker_swarm_scripts/create_docker_swarm.sh
+++ docker_swarm_scripts/create_docker_swarm.sh
@@ -3,15 +3,22 @@
 # Scriptin bulunduğu dizini alma
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/docker_swarm_setup.sh
+source $SCRIPT_DIR/set_swarm_node_variables.sh
 source $SCRIPT_DIR/../argument_parser.sh
 source $SCRIPT_DIR/../general_functions.sh
 
 parse_and_read_arguments "$@"
+set_swarm_node_variables || exit 1
 
-update_and_install_docker   # Docker'ı kur
+update_and_install_docker || exit 1 # Docker'ı kur
 
-enable_docker               # Docker'ı başlat ve etkinleştir
+enable_docker || exit 1 # Docker'ı başlat ve etkinleştir
 
-add_user_to_docker_group    # Kullanıcıyı docker grubuna ekle
+add_user_to_docker_group || exit 1 # Kullanıcıyı docker grubuna ekle
 
-initialize_docker_swarm     # Docker Swarm'ı başlat
+if [ "$IS_NODE_1" = true ]; then
+    initialize_docker_swarm_manager || exit 1 # Docker Swarm'ı manager olarak oluştur
+    write_swarm_worker_token || exit 1 # Başka cihaz eğer bu tokenla join olursa worker olur
+else
+    join_swarm_as_worker || exit 1 # Docker Swarm'a worker olarak katıl
+fi
--- docker_swarm_scripts/docker_swarm_setup.sh
+++ docker_swarm_scripts/docker_swarm_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/set_swarm_node_variables.sh
+source $SCRIPT_DIR/../argument_parser.sh
 # Paketleri güncelle ve Docker'ı kur
 update_and_install_docker() {
     # Docker kurulu mu kontrol et
@@ -62,15 +63,70 @@ add_user_to_docker_group() {
 }
 
 # Docker Swarm'ı başlat
-initialize_docker_swarm() {
-    set_swarm_node_variables
+initialize_docker_swarm_manager() {
     # Docker Swarm zaten başlatılmış mı kontrol et
     if ! docker info | grep -q 'Swarm: active'; then
         echo "Docker Swarm başlatılıyor..."
         docker swarm init --advertise-addr $CURRENT_NODE_IP || return 1
         echo "Docker Swarm başarıyla başlatıldı."
     else
-        echo "Docker Swarm zaten başlatılmış."
+        echo "Docker Swarm manager zaten ilklendirilmiş."
     fi
+    return 0
+}
+
+write_swarm_worker_token() {
+    # Swarm Worker Token'ını al
+    local SWARM_WORKER_TOKEN
+    SWARM_WORKER_TOKEN=$(docker swarm join-token -q worker)
+    if [ -z "$SWARM_WORKER_TOKEN" ]; then
+        echo "Hata: Swarm Worker Token alınamadı."
+        return 1
+    fi
+
+    # ARGUMENT_CFG_FILE değişkeninin tanımlı olduğundan emin olun
+    if [ -z "$ARGUMENT_CFG_FILE" ]; then
+        echo "Hata: ARGUMENT_CFG_FILE değişkeni tanımlı değil."
+        return 1
+    fi
+
+    # Token'ı arguments.cfg dosyasına ekle veya güncelle
+    update_config_file "DOCKER_SWARM_WORKER_TOKEN" "$ARGUMENT_CFG_FILE" "$SWARM_WORKER_TOKEN"
+
+    # İşlemin başarısını kontrol et
+    if [ $? -ne 0 ]; then
+        echo "Hata: Worker Token arguments.cfg dosyasına yazılamadı."
+        return 1
+    fi
+
+    echo "Swarm Worker Token başarıyla arguments.cfg dosyasına yazıldı."
+    return 0
+}
+
+# manager olan Docker Swarm cluster'ına katıl
+join_swarm_as_worker() {
+    DOCKER_SWARM_JOIN_PORT=2377
+    # DOCKER_SWARM_WORKER_TOKEN tanımlı mı kontrol et
+    if [ -z "$DOCKER_SWARM_WORKER_TOKEN" ]; then
+        echo "Hata: DOCKER_SWARM_WORKER_TOKEN tanımlı değil."
+        return 1
+    fi
+
+    # NODE1_IP tanımlı mı kontrol et
+    if [ -z "$NODE1_IP" ]; then
+        echo "Hata: NODE1_IP tanımlı değil."
+        return 1
+    fi
+
+    echo "Worker düğüm Swarm cluster'ına katılıyor..."
+
+    # Swarm'a katılma komutu
+    sudo docker swarm join --token "$DOCKER_SWARM_WORKER_TOKEN" "$NODE1_IP:$DOCKER_SWARM_JOIN_PORT"
+    if [ $? -ne 0 ]; then
+        echo "Hata: Swarm cluster'a katılım başarısız oldu."
+        return 1
+    fi
+
+    echo "Başarıyla Swarm cluster'ına katıldınız."
     return 0
 }
